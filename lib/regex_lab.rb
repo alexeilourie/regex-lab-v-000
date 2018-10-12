e2b447b87c7c462ff05f4cf25c8a6e3a1711689d
Alexei Lourie <alexeilourie@gmail.com>
2018-10-12 01:19:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  return text.scan(/\b[A-Z]\w*\S\b/)
+  if text.scan(/^[A-Z]/).any?&&text.scan(/\S$/)
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
