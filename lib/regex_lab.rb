36055e6a2eb50ac3739dc3d50b9c459417de7582
Alexei Lourie <alexeilourie@gmail.com>
2018-10-12 01:41:37 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  arr = phone.scan(/[0-9]/)[
+  arr = phone.scan(/[0-9]/)
   if arr[9] == nil
     return false
   else
