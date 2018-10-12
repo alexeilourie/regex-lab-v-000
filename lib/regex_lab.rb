7b373d356230e7dca3b396cc6e26e3ad0e002ea6
Alexei Lourie <alexeilourie@gmail.com>
2018-10-12 00:11:01 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\A[aeiouAEIOU]/).any?
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  return text.scan(/\A(un)|\z(ing)/)
 end
 
 def words_five_letters_long(text)
